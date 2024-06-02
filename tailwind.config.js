4613a77e06179a1afd0aca06e097299367161c08
Johanh0 <johanherrera@Johans-MacBook-Pro.local>
2024-06-02 10:54:42 -0400

    Tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#202632",
+        secondary: "#4EFEA5",
+        tertiary: "#303A49",
+      },
+    },
+  },
+  plugins: [],
+};
